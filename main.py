6318c8368f7994d7db218d18e57135472a91a56c
Sanjay <114412909+SANJAYGudHrt@users.noreply.github.com>
2023-07-19 17:51:13 +0530

    Add files via upload
    The AI-Powered Chatbot is a web application that leverages the GPT-3.5 API from OpenAI to provide natural language conversation capabilities. Users can interact with the chatbot by entering their questions or topics of interest. The chatbot performs spell-checking and grammar correction on the input text to ensure accurate communication with the language model.
    
    The chatbot then sends the user's input to the GPT-3.5 engine, which generates a response based on the provided prompt. The model uses state-of-the-art language understanding and generation capabilities to provide insightful and contextually relevant answers to user queries.
    
    Users can receive responses to their questions in real-time, making the application suitable for a wide range of use cases, such as information retrieval, natural language understanding, and human-like conversation experiences. The chatbot's user-friendly interface, combined with powerful AI capabilities, makes it an effective tool for exploring and understanding the potential of cutting-edge language models.

---
+++ main.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import openai
+from spellchecker import SpellChecker
+
+
+def correct_spelling(input_text):
+    if not input_text:
+        return input_text
+
+    spell = SpellChecker()
+    words = input_text.split()
+    corrected_words = [spell.correction(word) for word in words]
+    return " ".join(corrected_words)
+
+
+def chat_with_gpt(input_topic):
+    # Correct the input sentence for spelling mistakes
+    corrected_topic = correct_spelling(input_topic)
+
+    # Make API call to ChatGPT
+    api_key = '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'
+    openai.api_key = api_key
+
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",  # Use GPT-3.5 engine
+            prompt=corrected_topic,
+            max_tokens=100
+        )
+
+        if response and 'choices' in response and len(response['choices']) > 0:
+            answer = response['choices'][0]['text'].strip()
+            return answer
+    except Exception as e:
+        st.error(f"Error in API call: {e}")
+
+    return "Error in API call. Please try again later."
+
+
+def main():
+    st.title("Chatbot with GPT-3.5 API and Spell Check")
+
+    # Get the input topic from the user
+    input_topic = st.text_input("Enter your topic:")
+
+    if st.button("Get Answer"):
+        if input_topic:
+            # Chat with GPT and get the answer
+            answer = chat_with_gpt(input_topic)
+            st.subheader("Answer (less than 100 words):")
+            st.write(answer)
+        else:
+            st.warning("Please enter a topic to get an answer.")
+
+
+if __name__ == "__main__":
+    main()
